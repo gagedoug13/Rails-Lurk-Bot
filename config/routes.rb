13c5c184b1f35ed7693354759c6f3f252f0da97c
Gage Thornberry <gagedoug13@gmail.com>
2020-10-12 20:30:17 -1100

    added cors gemfile and allowed *. working on getting users from db

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :streams
-  resources :users
+  resources 'users'
+  get "/users", to: "users#users"
   get 'welcome/index'
   resources :articles
   root 'welcome#index'
